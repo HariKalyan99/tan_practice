5b25b30366a0cdba743c21c17ceaca827322bf8c
HariKalyan99 <harikalyan99tronics@gmail.com>
2024-07-29 17:20:27 +0530

    initial commit

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,25 @@
+export const postSignup = async(request, response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+export const postLogin = async(request, response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+export const postLogout = async(request, response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
