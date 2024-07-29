f57efd909b20bdc1f64b82838c889e48c06e224c
HariKalyan99 <harikalyan99tronics@gmail.com>
2024-07-29 19:24:23 +0530

    mongo server connections

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv'
 
+import authRoutes from './routes/auth.routes.js';
+import { connectToMongo } from './DB/connectToMongo.js';
+dotenv.config();
 
 const app = express();
 
 app.use("/api/auth", authRoutes)
 
 app.listen(8081, () => {
-    console.log(`Server is running on ${8081}`)
+    console.log(`Server is running on ${8081}`);
+    connectToMongo();
 })
